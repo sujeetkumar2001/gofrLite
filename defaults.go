18eaf9ae43b8b64a4a3fb6d93b1cf398672e8a24
Sujeet Kumar <snhacker_9@172-0-6-203.lightspeed.brhmal.sbcglobal.net>
2024-11-23 13:09:28 +0530

    added defaults init

---
+++ defaults.go
@@ -0,0 +1,64 @@
+package gofrLite
+
+import (
+	"net/http"
+	"reflect"
+)
+
+func setDefaults(v reflect.Value) error {
+	t := v.Type()
+
+	if t.Kind() == reflect.Ptr {
+		t = t.Elem()
+		v = v.Elem()
+	}
+
+	for i := 0; i < t.NumField(); i++ {
+		fieldType := t.Field(i)
+		field := v.Field(i)
+
+		if fieldType.Anonymous {
+			err := setAnonymousDefaults(field)
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		tagValue := fieldType.Tag.Get(string(DefaultTag))
+		if tagValue == "" {
+			continue
+		}
+
+		if !field.IsZero() {
+			continue
+		}
+
+		err := bindValue(tagValue, field)
+		if err != nil {
+			return http.ErrorInvalidParam{Params: []string{tagValue}}
+		}
+	}
+
+	return nil
+}
+
+func setAnonymousDefaults(field reflect.Value) error {
+	if field.Kind() == reflect.Ptr {
+		if field.IsNil() {
+			field.Set(reflect.New(field.Type().Elem()))
+		}
+	} else {
+		if field.CanAddr() {
+			field = field.Addr()
+		}
+	}
+
+	err := setDefaults(field)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gofrHackathon/gofrLite
+
+go 1.22.5
+
+require gofr.dev v1.27.1 // indirect
